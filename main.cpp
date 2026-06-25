cbd79479d73f689a44cacb6faae080e0b9a99123
Ilhumsarwar <ilhumsarwar@gmail.com>
2026-06-25 12:06:38 +0600

    food spawn and snake growth

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<SDL2/SDL.h>
+#include<time.h>
 
 //defines
-const int WINDOW_WIDTH = 640;
-const int WINDOW_HEIGHT = 640;
+const int WINDOW_WIDTH = 500;
+const int WINDOW_HEIGHT = 500;
 const char* WINDOW_TITLE = "Snake Game";
-#define NUM_COLS 20 //no. of columns in  a grid
-#define NUM_ROWS 20
+#define NUM_COLS 25 //no. of columns in  a grid
+#define NUM_ROWS 25
 #define CELL_SIZE (WINDOW_WIDTH / NUM_COLS) //size of each cell in pixels
 #define MAX_SNAKE_LENGTH (NUM_COLS * NUM_ROWS) 
 //direction constanst for easier understanding
@@ -20,8 +22,12 @@ int gameisrunning = 0;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 int last_frame_time = 0;  
-const int FPS = 10; // to change snake speed increase fps value
-const int FRAME_TARGET = 1000 / FPS;
+const int FPS = 5; // to change snake speed increase fps value
+const int FRAME_TARGET = 500 / FPS;
+    //food positions
+int food_col;
+int food_row;
+
 
 // snake data
 typedef struct{
@@ -48,7 +54,7 @@ int initialize_window(){
         SDL_WINDOWPOS_CENTERED,
         WINDOW_WIDTH,
         WINDOW_HEIGHT,
-        SDL_WINDOW_BORDERLESS
+        0
     );
 
     if(!window){
@@ -64,6 +70,60 @@ int initialize_window(){
 
     return 1;
 }
+
+// draws a single cell on the grid
+void draw_cell(int col,int row, int r,int g,int b){
+    //converts grid position to pixel position
+    SDL_Rect cell;
+    cell.x = col* CELL_SIZE + 1; // +1 , -2 so that grid lines are visible (optional)
+    cell.y = row*CELL_SIZE + 1;
+    cell.w = CELL_SIZE - 2;
+    cell.h = CELL_SIZE -2;
+
+    SDL_SetRenderDrawColor(renderer, r,g,b,255);//colour change
+    SDL_RenderFillRect(renderer,&cell); // draws the filled rectangle
+}
+
+// food spawning using random seed for each restart
+void spawn_food(void){
+    int flag = 0;
+    while(!flag){
+        food_col = rand() % NUM_COLS;
+        food_row = rand() % NUM_ROWS;
+        
+        for(int i=0;i<snake_length;i++){
+        flag = 1;
+        if(snake[i].row==food_col && snake[i].col == food_row){flag = 0;break;}
+        }
+    }
+    if(flag==1) draw_cell(food_col,food_row,255,0,0);//red food
+}
+
+// draw the snake 
+void draw_snake(void){
+    for(int i=0;i<snake_length;i++){
+        //head is dark green
+        if(i==0) draw_cell(snake[i].col,snake[i].row,0,255,0);
+        //body is light green
+        else draw_cell(snake[i].col,snake[i].row,0,150,0);
+    }
+}
+
+void draw_grid(){
+    SDL_SetRenderDrawColor(renderer,30,30,30,255);//very dark grey 
+    //draw vertical lines (top to bottom)
+    for(int col = 0;col<NUM_COLS;col++){
+        int x = col * CELL_SIZE;
+        SDL_RenderDrawLine(renderer,x,0,x,WINDOW_HEIGHT);
+    }
+
+    // horitzontal line left to right
+    for(int row = 0;row<NUM_ROWS; row++){
+        int y = row * CELL_SIZE;
+        SDL_RenderDrawLine(renderer,0,y,WINDOW_WIDTH,y);
+    }
+}
+
 
 void setup(void){
     //snake data will be initialized here
@@ -83,6 +143,10 @@ void setup(void){
     //tail
     snake[2].col = NUM_COLS/2 - 2; 
     snake[2].row = NUM_ROWS/2;
+
+    // using srand so that every time the pattern changes
+    srand(time(NULL));
+    spawn_food();
 }
 
 void process_input(){
@@ -134,6 +198,14 @@ void move_snake(void){
     snake[0] = new_head;
     snake[0].col = (snake[0].col + NUM_COLS) % NUM_COLS;
     snake[0].row = (snake[0].row + NUM_ROWS)% NUM_ROWS;
+
+    //increase length after eating
+    if(snake[0].col == food_col && snake[0].row == food_row){
+        if(snake_length< MAX_SNAKE_LENGTH){
+            snake_length++;
+        }
+        spawn_food();
+    }
 }
 
 void update(void){
@@ -144,42 +216,6 @@ void update(void){
 
     // snake movement here
     move_snake();
-}
-// draws a single cell on the grid
-void draw_cell(int col,int row, int r,int g,int b){
-    //converts grid position to pixel position
-    SDL_Rect cell;
-    cell.x = col* CELL_SIZE + 1; // +1 , -2 so that grid lines are visible (optional)
-    cell.y = row*CELL_SIZE + 1;
-    cell.w = CELL_SIZE - 2;
-    cell.h = CELL_SIZE -2;
-
-    SDL_SetRenderDrawColor(renderer, r,g,b,255);//colour change
-    SDL_RenderFillRect(renderer,&cell); // draws the filled rectangle
-}
-// draw the snake 
-void draw_snake(void){
-    for(int i=0;i<snake_length;i++){
-        //head is dark green
-        if(i==0) draw_cell(snake[i].col,snake[i].row,0,255,0);
-        //body is light green
-        else draw_cell(snake[i].col,snake[i].row,0,150,0);
-    }
-}
-
-void draw_grid(){
-    SDL_SetRenderDrawColor(renderer,30,30,30,255);//very dark grey 
-    //draw vertical lines (top to bottom)
-    for(int col = 0;col<NUM_COLS;col++){
-        int x = col * CELL_SIZE;
-        SDL_RenderDrawLine(renderer,x,0,x,WINDOW_HEIGHT);
-    }
-
-    // horitzontal line left to right
-    for(int row = 0;row<NUM_ROWS; row++){
-        int y = row * CELL_SIZE;
-        SDL_RenderDrawLine(renderer,0,y,WINDOW_WIDTH,y);
-    }
 }
 
 void render(void){
@@ -194,7 +230,9 @@ void render(void){
 
     //draw the snake
     draw_snake();
-
+    // draw the food
+    draw_cell(food_col,food_row,255,0,0); // red food
+    
     SDL_RenderPresent(renderer);
 }
 
